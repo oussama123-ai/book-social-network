aaf87530cf36646325674d3718ca19d3aea2acd8
oussama123-ai <oussamaothmani16@gmail.com>
2024-07-28 03:16:18 +0200

    first commit

---
+++ book-network/src/main/java/com/alibou/book/auth/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.oussama.book.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+    private String token;
+}
---
+++ book-network/src/main/java/com/alibou/book/history/BookTransactionHistory.java
@@ -0,0 +1,31 @@
+package com.oussama.book.history;
+
+import com.oussama.book.book.Book;
+import com.oussama.book.common.BaseEntity;
+import com.oussama.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
